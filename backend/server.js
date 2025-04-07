515793aff7650d32d009042cba85548d6d22e333
Sadavarte Shraddha <shraddhasadavarte24@gmail.com>
2025-04-07 15:31:32 +0530

    basic mongodb connection created

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+// import authRoutes from "./routes/authRoutes.js";
+// import projectRoutes from "./routes/projectRoutes.js";
+// import taskRoutes from "./routes/taskRoutes.js";
+// import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/projects", projectRoutes);
+// app.use("/api/tasks", taskRoutes);
+// app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
